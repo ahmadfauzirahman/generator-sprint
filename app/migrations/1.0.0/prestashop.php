baa689801c3198879ef2634472cee5e06df4b333
ahmad fauzi rahman <ahmadfauzirahman99@gmail.com>
2019-04-04 15:54:04 +0700

    Migrate

---
+++ app/migrations/1.0.0/prestashop.php
@@ -0,0 +1,102 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class PrestashopMigration_100
+ */
+class PrestashopMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('prestashop', [
+                'columns' => [
+                    new Column(
+                        'prestashopId',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'prestashopNama',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 50,
+                            'after' => 'prestashopId'
+                        ]
+                    ),
+                    new Column(
+                        'prestashopTanggal',
+                        [
+                            'type' => Column::TYPE_DATE,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'prestashopNama'
+                        ]
+                    ),
+                    new Column(
+                        'prestashopStatus',
+                        [
+                            'type' => Column::TYPE_CHAR,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'prestashopTanggal'
+                        ]
+                    ),
+                    new Column(
+                        'prestashopFoto',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'prestashopStatus'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['prestashopId'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '5',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'latin1_swedish_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
